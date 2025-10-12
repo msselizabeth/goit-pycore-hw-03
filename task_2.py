d1c9c4e5b86503d67ee2e75f06c9749f1b3fb075
msselizabeth <lizagotovitryby@gmail.com>
2025-10-11 22:02:53 -0400

    completed task_2

---
+++ task_2.py
@@ -0,0 +1,28 @@
+"""
+Щоб виграти головний приз лотереї, необхідний збіг кількох номерів на лотерейному квитку з числами, що випали випадковим чином і в певному діапазоні під час чергового тиражу. Наприклад, необхідно вгадати шість чисел від 1 до 49 чи п'ять чисел від 1 до 36 тощо.
+
+Вам необхідно написати функцію get_numbers_ticket(min, max, quantity), яка допоможе генерувати набір унікальних випадкових чисел для таких лотерей. Вона буде повертати випадковий набір чисел у межах заданих параметрів, причому всі випадкові числа в наборі повинні бути унікальні.
+"""
+
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    # Validating params
+    if (min < 1) or (max > 1000) or not (min <= quantity <= max):
+        return []
+
+    #  create a list of nums in min-max range
+    nums = list(range(min, max + 1))
+    
+    # Random choice of uniqe nums from the list, and sorting
+    result = random.sample(nums, k=quantity)
+    result.sort()
+
+    return result
+
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+lottery_numbers = get_numbers_ticket(0, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers) # []
