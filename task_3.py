5e88b1b261573e1a5dd6a9265618dd9da693e912
msselizabeth <lizagotovitryby@gmail.com>
2025-10-12 14:29:09 -0400

    completed task_3

---
+++ task_3.py
@@ -0,0 +1,28 @@
+"""
+Розробіть функцію normalize_phone(phone_number), що нормалізує телефонні номери до стандартного формату, залишаючи тільки цифри та символ '+' на початку. Функція приймає один аргумент - рядок з телефонним номером у будь-якому форматі та перетворює його на стандартний формат, залишаючи тільки цифри та символ '+'. Якщо номер не містить міжнародного коду, функція автоматично додає код '+38' (для України). Це гарантує, що всі номери будуть придатними для відправлення SMS.
+"""
+
+import re
+
+raw_numbers = [
+    "067\t123 4567",
+    "(095) 234-5678\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+
+def normalize_phone(phone_number):
+    # Clean initial number : leave only digits and +
+    phone_number = re.sub(r"[^\d+]", "", phone_number)
+    # Check if number starts from +380, 38, 0 and update if nedeed 
+    phone_number = re.sub(r"^(?:\+?380|0)", "+380", phone_number)
+    return phone_number
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
