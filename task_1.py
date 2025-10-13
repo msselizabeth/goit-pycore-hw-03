e7073a7e7864a4abb2a041004412b2b5f80cd2b2
msselizabeth <lizagotovitryby@gmail.com>
2025-10-13 15:27:25 -0400

    fixed task_2

--- task_1.py
+++ task_1.py
@@ -4,20 +4,22 @@
 
 from datetime import datetime
 
+
 def get_days_from_today(date):
-    # parsing the date
     try:
+        # parsing the date
         parsed_date = datetime.strptime(date, "%Y-%m-%d").date()
     except ValueError:
-        raise ValueError(f"Invalid date format: {date}. Expected YYYY-MM-DD")
-
+        print(f"Invalid date format: {date}. Expected YYYY-MM-DD")
+        return None
+    
     # getting current date
     now = datetime.now().date()
 
-    # return thr  difference, if date is less than today, and if not return 0 
-    return (parsed_date - now).days if parsed_date < now else 0
+    return (parsed_date - now).days
 
 
-print(get_days_from_today("2020-10-09"))  # -1828 (Current 2025-10-11)  
-print(get_days_from_today("2025-10-11"))  # 0 (Current 2025-10-11)
-# get_days_from_today("99") # ValueError: Invalid date format: 99. Expected YYYY-MM-DD
+print(get_days_from_today("2020-10-09"))
+print(get_days_from_today("2025-10-11"))
+print(get_days_from_today("2025-10-30"))
+print(get_days_from_today("2025.10.10"))
